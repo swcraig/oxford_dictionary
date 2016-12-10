836c62d4054c38ea20fada4391ebda07d14aa78d
swcraig <sebastian.w.craig@gmail.com>
2016-12-09 22:44:13 -0800

    Add attributes to Sense

--- lib/oxford_dictionary/api_objects/sense.rb
+++ lib/oxford_dictionary/api_objects/sense.rb
@@ -4,9 +4,14 @@ require 'virtus'
 class Sense
   include Virtus.model
   attribute :id, String
+  attribute :cross_reference_markers, Array[String]
+  attribute :cross_references, Array[OpenStruct]
+
   attribute :definitions, Array[String]
   attribute :examples, Array[OpenStruct]
   attribute :registers, Array[String]
+
+  attribute :subsenses, Array[Sense]
 
   attribute :antonyms, Array[OpenStruct]
   attribute :synonyms, Array[OpenStruct]
