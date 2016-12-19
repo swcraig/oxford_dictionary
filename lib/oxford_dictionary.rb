0e51a417a841b855981226e562ba6f05d2123548
swcraig <sebastian.w.craig@gmail.com>
2016-12-18 23:30:52 -0800

    Remove useless requires

--- lib/oxford_dictionary.rb
+++ lib/oxford_dictionary.rb
@@ -1,11 +1,5 @@
 require 'oxford_dictionary/client'
-require 'oxford_dictionary/error'
-require 'oxford_dictionary/request'
 require 'oxford_dictionary/version'
-require 'oxford_dictionary/endpoints/entry_endpoint'
-require 'oxford_dictionary/endpoints/inflection_endpoint'
-require 'oxford_dictionary/endpoints/search_endpoint'
-require 'oxford_dictionary/endpoints/wordlist_endpoint'
 
 # Adds some aliasing
 module OxfordDictionary
