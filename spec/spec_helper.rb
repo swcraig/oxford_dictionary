32f6f5e63b0ec7ef7912fd24e5f6bf0dc9d20d97
swcraig <swcraig.code@gmail.com>
2019-05-07 19:41:57 -0700

    Setup VCR in spec_helper
    Instead of doing all the crazy hand-rolled request stubbing that I
    thought was OK in 3rd year, we can use VCR to do the same since it's
    actually built for that.
    
    We can use this going forward for the V2 release.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,12 @@ SimpleCov.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'oxford_dictionary'
 require 'webmock/rspec'
+require 'vcr'
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
 
 TEST_API_ID = 'ID'.freeze
 TEST_API_KEY = 'SECRET'.freeze
