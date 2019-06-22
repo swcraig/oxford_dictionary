6ba2da763cfa2a43260f9e74d4b41a0a9572461a
swcraig <swcraig.code@gmail.com>
2019-06-22 12:41:17 -0700

    Add Entries endpoint to Client
    This continues support for both V1 and V2 calls to the API without the
    need for a code change. This _should_ give callers some time to move
    their usage to the new V2 approach (using named parameters, check
    `OxfordDictionary::Endpoints::Entries` to see the new interface).
    
    The general idea for upgrade is 1.1.0 for the V2 entries support.
    Subsequent minor versions will likely include the rest of the endpoints
    (Lemmas, Thesaurus, etc.) with 2.0.0 finally removing all V1 interfaces.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
-describe 'Client' do
+RSpec.describe OxfordDictionary::Client do
+  let(:app_id) { 'ID' }
+  let(:app_key) { 'KEY' }
+
   describe '#new' do
     it 'requires an argument' do
       expect { OxfordDictionary::Client.new(nil) }
@@ -15,6 +18,23 @@ describe 'Client' do
     it 'requires both id and key' do
       expect { OxfordDictionary.new(app_id: 'ID') }
         .to raise_error(ArgumentError, 'API id and key required.')
+    end
+  end
+
+  describe '#entry' do
+    let(:client) { described_class.new(app_id: app_id, app_key: app_key) }
+    subject { client.entry(args) }
+
+    context 'when the argument is a Hash' do
+      let(:args) { { word: 'ace', dataset: 'en-us', params: {} } }
+
+      it 'calls the Entries endpoint with correct arguments' do
+        expect_any_instance_of(OxfordDictionary::Endpoints::Entries).
+          to receive(:entry).
+          with(word: args[:word], dataset: args[:dataset], params: args[:params])
+
+        subject
+      end
     end
   end
 end
