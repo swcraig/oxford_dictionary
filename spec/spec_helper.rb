bae1f254cf0cb96f2d97b6ce0df1a91c2213ff52
swcraig <swcraig.code@gmail.com>
2019-06-22 10:58:22 -0700

    Filter out app IDs and keys in VCR cassettes
    My Oxford Dictionaries API app ID and key are the most important thing I
    have been associated with in my entire life.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,8 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
+  config.filter_sensitive_data('APP_ID') { |request| ENV['APP_ID'] }
+  config.filter_sensitive_data('APP_KEY') { |request| ENV['APP_KEY'] }
 end
 
 TEST_API_ID = 'ID'.freeze
