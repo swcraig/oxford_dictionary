81a64f0c5f443f041b957f15cfc965c7cf14a3e3
swcraig <swcraig.code@gmail.com>
2019-06-29 22:21:09 -0700

    Remove V1 support in spec_helper
    We use VCR instead of this hand-rolled stubbing nonsense.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,38 +12,6 @@ VCR.configure do |config|
   config.filter_sensitive_data('APP_KEY') { |request| ENV['APP_KEY'] }
 end
 
-TEST_API_ID = 'ID'.freeze
-TEST_API_KEY = 'SECRET'.freeze
-ACCEPT = 'application/json'.freeze
-HEADERS = { 'Accept' => ACCEPT, 'App-Id' => 'ID', 'App-Key' => 'SECRET' }.freeze
-
-def stub_get(path, fixture_name)
-  headers = HEADERS
-  stub_request(:get, api_url(path))
-    .with(headers: headers)
-    .to_return(status: 200, body: fixture(fixture_name))
-end
-
-def stub_error(path, fixture_name)
-  headers = HEADERS
-  stub_request(:get, api_url(path))
-    .with(headers: headers)
-    .to_return(status: 404, body: fixture(fixture_name))
-end
-
-def fixture_path(file = nil)
-  path = File.expand_path('../fixtures', __FILE__)
-  file.nil? ? path : File.join(path, file)
-end
-
-def fixture(file)
-  File.read(fixture_path(file))
-end
-
-def api_url(path)
-  "#{OxfordDictionary::DeprecatedRequest::BASE}/#{path}"
-end
-
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
