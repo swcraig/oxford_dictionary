3141c449f0c6306a2fa35f001926cf55b8c0087f
swcraig <swcraig.code@gmail.com>
2019-06-29 21:56:03 -0700

    Delegate V1 `#search` usage in `Client`
    Oxford Dictionaries is upgrading their API. As part of the V2 changes
    this library is going to use a named parameters approach to accessing
    the endpoint. Reference
    https://github.com/swcraig/oxford-dictionary/pull/15 for the new
    interface.
    
    https://developer.oxforddictionaries.com/version2

--- spec/endpoint_specs/search_endpoint_spec.rb
+++ spec/endpoint_specs/search_endpoint_spec.rb
@@ -1,40 +1,51 @@
 require 'spec_helper'
 
-describe OxfordDictionary::Endpoints::SearchEndpoint do
-  before do
-    stub_get('search/en?q=vapid&prefix=false', 'search_vapid.json')
-    stub_get(
-      'search/en?q=condition&prefix=true',
-      'search_condition_prefix.json'
-    )
-    stub_get(
-      'search/en/translations=id?q=eye&prefix=false',
-      'search_eye_id.json'
-    )
+describe 'V1 search delegations' do
+  let(:client) do
+    OxfordDictionary.new(app_id: ENV['APP_ID'], app_key: ENV['APP_KEY'])
   end
-  let(:client) { OxfordDictionary.new(app_id: 'ID', app_key: 'SECRET') }
+  let(:response_double) { double(body: {}.to_json) }
 
   context '#search without filters' do
     let(:resp) { client.search('vapid') }
-    it 'is a search request' do
-      expect(resp.results).to be_an Array
-      expect(resp.results[0].match_string).to eq('vapid')
+
+    it 'makes a request to the proper V2 endpoint' do
+      VCR.use_cassette('v1_search') do
+        expect_any_instance_of(OxfordDictionary::Request).
+          to receive(:get).
+          with(uri: URI('search/en-gb?q=vapid')).
+          once.
+          and_return(response_double)
+        resp
+      end
     end
   end
 
   context '#search as a prefix' do
     let(:resp) { client.search('condition', prefix: true) }
-    it 'searches as a prefix' do
-      expect(resp.results[0].match_type).to eq('inflection')
-      expect(resp.results[0].word).to eq('condition')
+    it 'makes a request to the proper V2 endpoint' do
+      VCR.use_cassette('v1_search_prefix') do
+        expect_any_instance_of(OxfordDictionary::Request).
+          to receive(:get).
+          with(uri: URI('search/en-gb?prefix=true&q=condition')).
+          once.
+          and_return(response_double)
+        resp
+      end
     end
   end
 
   context '#search with translations argument' do
     let(:resp) { client.search('eye', translations: 'id') }
-    it 'has the desired translations' do
-      expect(resp.results.size).to eq(2)
-      expect(resp.results[0].id).to eq('eye')
+    it 'makes a request to the proper V2 endpoint' do
+      VCR.use_cassette('v1_search_translation') do
+        expect_any_instance_of(OxfordDictionary::Request).
+          to receive(:get).
+          with(uri: URI('search/translations/en-gb/id?q=eye')).
+          once.
+          and_return(response_double)
+        resp
+      end
     end
   end
 end
