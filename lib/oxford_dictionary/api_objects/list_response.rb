9bc5862db2921732cded173723515ad12726c1b4
swcraig <swcraig.code@gmail.com>
2019-06-29 22:23:31 -0700

    Remove all V1 entities
    Instead of parsing the JSON response from the API into one of these
    entities we instead parse the response into a nested OpenStruct.
    
    Reference OxfordDictionary::Deserialize to see the implementation.

--- lib/oxford_dictionary/api_objects/list_response.rb
+++
@@ -1,7 +0,0 @@
-require 'virtus'
-
-# Top level response (excluding metadata) from search and wordlist endpoints
-class ListResponse
-  include Virtus.model
-  attribute :results, Array[OpenStruct]
-end
