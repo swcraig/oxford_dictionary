02fb82cc6bd13d6c261fa951a2878a3bce078211
swcraig <swcraig.code@gmail.com>
2019-07-12 22:44:07 -0700

    Use dot on trailing in Client
    We've been using trailing dot syntax in other places.

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -7,18 +7,18 @@ RSpec.describe OxfordDictionary::Client do
 
   describe '#new' do
     it 'requires an argument' do
-      expect { OxfordDictionary::Client.new(nil) }
-        .to raise_error(ArgumentError, 'API id and key required.')
+      expect { OxfordDictionary::Client.new(nil) }.
+        to raise_error(ArgumentError, 'API id and key required.')
     end
 
     it 'requires a hash argument' do
-      expect { OxfordDictionary::Client.new('string') }
-        .to raise_error(ArgumentError, 'API id and key required.')
+      expect { OxfordDictionary::Client.new('string') }.
+        to raise_error(ArgumentError, 'API id and key required.')
     end
 
     it 'requires both id and key' do
-      expect { OxfordDictionary.new(app_id: 'ID') }
-        .to raise_error(ArgumentError, 'API id and key required.')
+      expect { OxfordDictionary.new(app_id: 'ID') }.
+        to raise_error(ArgumentError, 'API id and key required.')
     end
   end
 
