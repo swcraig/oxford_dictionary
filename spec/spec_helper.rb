8c8e62926f402e67b3751d585b687dc3092f429b
swcraig <swcraig.code@gmail.com>
2019-07-12 22:46:01 -0700

    Fix spec helper offenses
    RuboCop is always right.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,8 @@ require 'vcr'
 VCR.configure do |config|
   config.cassette_library_dir = 'fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('APP_ID') { |request| ENV['APP_ID'] }
-  config.filter_sensitive_data('APP_KEY') { |request| ENV['APP_KEY'] }
+  config.filter_sensitive_data('APP_ID') { ENV['APP_ID'] }
+  config.filter_sensitive_data('APP_KEY') { ENV['APP_KEY'] }
 end
 
 RSpec.configure do |config|
